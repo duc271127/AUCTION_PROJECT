98a39f18deeb29d50a5c46f60425853f869a8844
tduck_z <duc271127@gmail.com>
2026-04-21 14:04:27 +0700

    Delete src directory

--- src/main/java/com/example/auction/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.auction.repository;
-
-import com.example.auction.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
