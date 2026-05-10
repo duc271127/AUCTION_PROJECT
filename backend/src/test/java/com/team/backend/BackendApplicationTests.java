cda081a87155d30dda0df0286d45ba513aa25e9f
tduck_z <duc271127@gmail.com>
2026-05-10 22:47:44 +0700

    Update BackendApplicationTests.java

--- backend/src/test/java/com/team/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/team/backend/BackendApplicationTests.java
@@ -3,11 +3,10 @@ package com.team.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = BackendApplication.class) // ← thêm classes=
+@SpringBootTest
 class BackendApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
