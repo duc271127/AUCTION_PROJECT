a595765fb5457749d000d53887b7b4fffdf15af4
nguyenhongdang412007-lgtm <nguyenhongdang412007@gmail.com>
2026-05-20 22:19:05 +0700

    Update AutoBidService.java

--- backend/src/main/java/com/team/backend/service/AutoBidService.java
+++ backend/src/main/java/com/team/backend/service/AutoBidService.java
@@ -5,8 +5,15 @@ import com.team.backend.entity.AutoBid;
 import java.util.UUID;
 
 public interface AutoBidService {
-    AutoBid setAutoBid(UUID auctionId, UUID bidderId, double maxAmount);
-}
-public void cancelAutoBid(UUID auctionId, UUID bidderId) {
 
+    AutoBid setAutoBid(
+            UUID auctionId,
+            UUID bidderId,
+            double maxAmount
+    );
+
+    void cancelAutoBid(
+            UUID auctionId,
+            UUID bidderId
+    );
 }
