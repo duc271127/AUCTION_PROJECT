4e2977ad84afac43c3cc58a057b2d5b22fcad9a3
nguyenhongdang412007-lgtm <nguyenhongdang412007@gmail.com>
2026-05-20 22:23:30 +0700

    Update AutoBidServiceImpl.java\

--- backend/src/main/java/com/team/backend/service/impl/AutoBidServiceImpl.java
+++ backend/src/main/java/com/team/backend/service/impl/AutoBidServiceImpl.java
@@ -1,43 +1,30 @@
 package com.team.backend.service.impl;
 
 import com.team.backend.entity.AutoBid;
-import com.team.backend.exception.InvalidBidException;
-import com.team.backend.repository.AutoBidRepository;
 import com.team.backend.service.AutoBidService;
+
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
 @Service
 public class AutoBidServiceImpl implements AutoBidService {
 
-    private final AutoBidRepository autoBidRepository;
+    @Override
+    public AutoBid setAutoBid(
+            UUID auctionId,
+            UUID bidderId,
+            double maxAmount
+    ) {
 
-    public AutoBidServiceImpl(AutoBidRepository autoBidRepository) {
-        this.autoBidRepository = autoBidRepository;
+        return null;
     }
 
     @Override
-    @Transactional
-    public AutoBid setAutoBid(UUID auctionId, UUID bidderId, double maxAmount) {
-        if (auctionId == null || bidderId == null) {
-            throw new InvalidBidException("auctionId and bidderId are required");
-        }
+    public void cancelAutoBid(
+            UUID auctionId,
+            UUID bidderId
+    ) {
 
-        if (maxAmount <= 0) {
-            throw new InvalidBidException("maxAmount must be greater than 0");
-        }
-
-        AutoBid autoBid = autoBidRepository
-                .findByAuctionIdAndBidderId(auctionId, bidderId)
-                .orElseGet(AutoBid::new);
-
-        autoBid.setAuctionId(auctionId);
-        autoBid.setBidderId(bidderId);
-        autoBid.setMaxAmount(maxAmount);
-        autoBid.setActive(true);
-
-        return autoBidRepository.save(autoBid);
     }
 }
