ce1f064f40272ea6e6c95052accdde5b134f1741
manhLN <lenhomanh2007@gmail.com>
2026-05-21 11:03:07 +0700

    Update BidService.java

--- backend/src/main/java/com/team/backend/service/BidService.java
+++ backend/src/main/java/com/team/backend/service/BidService.java
@@ -7,13 +7,9 @@ import java.util.UUID;
 
 public interface BidService {
     BidTransaction placeBid(UUID auctionId, UUID bidderId, double amount);
-<<<<<<< Updated upstream
     List<BidTransaction> getBidHistory(UUID auctionId);
-=======
     List<BidHistoryDto> getBidHistory(UUID auctionId);
     Map<String, Object> getAuctionSummary(UUID auctionId);
     UUID getCurrentLeader(UUID auctionId);
     double getMinIncrement();
-
->>>>>>> Stashed changes
 }
